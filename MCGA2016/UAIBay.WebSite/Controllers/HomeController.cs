83c37e9c3c8f0529a798bf7728914db9f9d5ff33
HartridgeLucas <lucas.hartridge9@gmail.com>
2016-08-27 22:13:27 -0300

    Creando la Solucion de UAI Bay

---
+++ MCGA2016/UAIBay.WebSite/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UAIBay.WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Contacto()
+        {
+            return View();
+        }
+
+        public ActionResult NuestraInformacion()
+        {
+            return View();
+        }
+        public ActionResult FAQS()
+        {
+            return View();
+        }
+	}
+}
---
+++ MCGA2016/UAIBay.WebSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UAIBay.WebSite.MvcApplication" Language="C#" %>
