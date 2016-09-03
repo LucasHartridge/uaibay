369f5c9808047fb2751c3494e474ef7e6261130f
nicoquisamas <nicoquisamas@gmail.com>
2016-09-03 18:12:07 -0300

    insertando ORM

---
+++ MCGA2016/UAIBay.WebSite/Controllers/ProductoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UAIBay.WebSite.Controllers
+{
+    public class ProductoController : Controller
+    {
+        //
+        // GET: /Account/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    [HttpPost]
+        public ActionResult VerProducto()
+        {
+            return View();
+        }
+	}
+}
