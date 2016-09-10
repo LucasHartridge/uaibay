9b0b2b18212259f49e03d12c4112e47470005e7b
nicoquisamas <nicoquisamas@gmail.com>
2016-09-10 18:43:14 -0300

    Implementando repository

---
+++ MCGA2016/UAIBay.ORM/Context/UAIBayContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UAIBay.ORM.Business;
+
+namespace UAIBay.ORM.Context
+{
+    public class UAIBayContext:DbContext
+    {
+        public UAIBayContext() : base("name=UAIBayAccess")
+        {
+
+        }
+
+        public virtual DbSet<Categoria> Categorias { get; set; }
+
+    }
+}
